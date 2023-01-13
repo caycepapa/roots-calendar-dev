2128971d5a1886413626704a8b3d53a60fe4f047
cayce <cayce@danguri.com>
2023-01-13 17:46:07 +0900

    設定画面調整

--- roots-calender.php
+++ roots-calender.php
@@ -21,15 +21,17 @@ class CustomMetaTable {
 
     function cmt_activate() {
         global $wpdb;
-        $cmt_db_version = '4.0';
+        $cmt_db_version = '8.0';
 
         $installed_ver = get_option( 'cmt_meta_version' );
 
         if( $installed_ver != $cmt_db_version ) {
             $sql = "CREATE TABLE " . $this->table_name . " (
                 id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                event_name text NOT NULL,
-                event_color text NOT NULL,
+                state_name text NOT NULL,
+                state_color text NOT NULL,
+                state_txt text NOT NULL,
+                state_mark text NOT NULL,
                 UNIQUE KEY id (id)
             )
             CHARACTER SET 'utf8';";
@@ -71,12 +73,14 @@ class RootsCalender{
             $table_name = $wpdb->prefix . RC_Config::SETTING_TABLE;
 
             // setting add
-            if(isset($_POST['event_name']) || isset($_POST['event_color'])){
+            if(isset($_POST['state_name']) || isset($_POST['state_color']) || isset($_POST['state_mark']) || isset($_POST['state_txt']) ){
                 $wpdb->insert(
                     $table_name,
                     array(
-                        'event_name' => $_POST['event_name'],
-                        'event_color' => $_POST['event_color'],
+                        'state_name' => $_POST['state_name'],
+                        'state_color' => $_POST['state_color'],
+                        'state_mark' => $_POST['state_mark'],
+                        'state_txt' => $_POST['state_txt'],
                     )
                 );
             }
@@ -88,16 +92,18 @@ class RootsCalender{
                     if($cal_setting['delete']){
                         $wpdb->delete(
                             $table_name,
-                            array( 'id' =>  $cal_setting['event_id']) 
+                            array( 'id' =>  $cal_setting['state_id']) 
                         );
                     }else{
                         $wpdb->update(
                             $table_name,
                             array(
-                                'event_name' => $cal_setting['event_name'],
-                                'event_color' => $cal_setting['event_color'],
+                                'state_name' => $cal_setting['state_name'],
+                                'state_color' => $cal_setting['state_color'],
+                                'state_mark' => $cal_setting['state_mark'],
+                                'state_txt' => $cal_setting['state_txt'],
                             ),
-                            array( 'id' =>  $cal_setting['event_id']) 
+                            array( 'id' =>  $cal_setting['state_id']) 
                         );
                     }
                 }
@@ -111,8 +117,13 @@ class RootsCalender{
                 <hr class="wp-header-end">
                 <h2>新規登録</h2>
                 <form action='edit.php?post_type=<?php echo RC_Config::NAME;?>&page=<?php echo RC_Config::SETTING_NAME;?>' method='POST'>
-                    <input type="text" name="event_name" value="">
-                    <input type="color" name="event_color" value="">
+                    <input type="text" name="state_name" value="">
+                    <select name="state_mark">
+                        <option value="◯">◯</option>
+                        <option value="✕">✕</option>
+                    </select>
+                    <input type="color" name="state_color" value="">
+                    <input type="text" name="state_txt">
                     <input type="submit" value="追加">
                 </form>
                 <h2>一覧</h2>
@@ -123,9 +134,17 @@ class RootsCalender{
                         foreach($records as $record):
                     ?>
                             <div>
-                                <input type="text" name="cal_setting[<?php echo $record['id'];?>][event_name]" value="<?php echo $record['event_name'];?>">
-                                <input type="color" name="cal_setting[<?php echo $record['id'];?>][event_color]" value="<?php echo $record['event_color'];?>">
-                                <input type="hidden" name="cal_setting[<?php echo $record['id'];?>][event_id]" value="<?php echo $record['id'];?>">
+                                <input type="text" name="cal_setting[<?php echo $record['id'];?>][state_name]" value="<?php echo $record['state_name'];?>">
+                                <input type="color" name="cal_setting[<?php echo $record['id'];?>][state_color]" value="<?php echo $record['state_color'];?>">
+                                <input type="text" name="cal_setting[<?php echo $record['id'];?>][state_txt]" value="<?php echo $record['state_txt'];?>">
+                                <select name="cal_setting[<?php echo $record['id'];?>][state_mark]" id="">
+                                    <?php if($record['state_mark'] == '◯'): ?>
+                                        <option value="◯" selected>◯</option>
+                                    <?php else: ?>
+                                        <option value="✕" selected>✕</option>
+                                    <?php endif;?>
+                                </select>
+                                <input type="hidden" name="cal_setting[<?php echo $record['id'];?>][state_id]" value="<?php echo $record['id'];?>">
                                 <label><input type="checkbox" name="cal_setting[<?php echo $record['id'];?>][delete]">削除</label>
                             </div>
                     <?php
