0a9ef427dc8f613c97425a71eeeb585924a51782
cayce <cayce@danguri.com>
2023-01-24 13:45:42 +0900

    カレンダー入力画面スタイル調整

--- src/js/modules/create-modal-window.js
+++ src/js/modules/create-modal-window.js
@@ -9,25 +9,49 @@ export function open(date,meta,num){
 
     var addView = (date,meta,num) =>{
         eventDomContent += 
-            "<div>" +
-                "<p>"+date+"</p>" +
-                "<input type='text' name='"+ meta +"["+num+"][event_name]' value=''>" +
-                "<input type='text' name='"+ meta +"["+num+"][event_url]' value=''>" +
-                "<input type='color' name='"+ meta +"["+num+"][event_color]' value=''>" +
-                "<input type='submit' value='追加' name='save'>" + 
+            "<div class='rc-modal__wrap'>" +
+                "<p class='rc-modal__ttl'>"+date+"</p>" +
+                "<table class='rc-modal__inner'>" +
+                    "<tr>" +
+                    "<th>タイトル</th>" +
+                    "<td><input type='text' name='"+ meta +"["+num+"][event_name]' value=''></td>" +
+                    "</tr>" +
+                    "<tr>" +
+                    "<th>URL</th>" +
+                    "<td><input type='text' name='"+ meta +"["+num+"][event_url]' value=''></td>" +
+                    "</tr>" +
+                    "<tr>" +
+                    "<th>カラー</th>" +
+                    "<td><input type='color' name='"+ meta +"["+num+"][event_color]' value='#FF0000'></td>" +
+                    "</tr>" +
+                "</table>" +
+                "<div class='rc-modal__submit'><input type='submit' value='追加する' name='save'></div>" + 
                 "<a class='rc-modal__close'>閉じる</a>" +
             "</div>";
     }
 
     var rewriteView = (date,meta,num) =>{
         eventDomContent += 
-            "<div>" +
-                "<p>"+date+"</p>" +
-                "<input type='text' name='" + meta + "["+num+"][event_name]' value='"+ events_list_arry[num].event_name + "'>" +
-                "<input type='text' name='" + meta + "["+num+"][event_url]' value='" + events_list_arry[num].event_url + "'>" +
-                "<input type='color' name='" + meta + "["+num+"][event_color]' value='" + events_list_arry[num].event_color + "'>" +
-                "<input type='submit' value='更新' name='save'>" + 
-                "<a class='rc-modal__delete'>削除</a>" + 
+            "<div class='rc-modal__wrap'>" +
+                "<p class='rc-modal__ttl'>"+date+"</p>" +
+                "<table class='rc-modal__inner'>" +
+                "<tr>" +
+                "<th>タイトル</th>" +
+                "<td><input type='text' name='" + meta + "["+num+"][event_name]' value='"+ events_list_arry[num].event_name + "'></td>" +
+                "</tr>" +
+                "<tr>" +
+                "<th>URL</th>" +
+                "<td><input type='text' name='" + meta + "["+num+"][event_url]' value='" + events_list_arry[num].event_url + "'></td>" +
+                "</tr>" +
+                "<tr>" +
+                "<th>カラー</th>" +
+                "<td><input type='color' name='" + meta + "["+num+"][event_color]' value='" + events_list_arry[num].event_color + "'></td>" +
+                "</tr>" +
+                "</table>" +
+                "<div class='rc-modal__submit'><input type='submit' value='更新する' name='save'></div>" + 
+                "<div class='rc-modal__controller'>" +
+                    "<a class='rc-modal__delete'>削除</a>" + 
+                "</div>" +
                 "<a class='rc-modal__close'>閉じる</a>" +
             "</div>";
     }
