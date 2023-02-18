8e5f44b0dad205777ee57f30289789aa47f5a0e7
keishiro air <k.itagawa@roots.run>
2023-02-19 01:04:15 +0900

    カスタマイザーでのバグ調整

--- src/js/modules/create-cal.js
+++ src/js/modules/create-cal.js
@@ -1,6 +1,5 @@
 "use strict";
 
-import { forEach } from "lodash";
 import * as create_modal_window from "./create-modal-window";
 
 export function createFunc(){
@@ -12,184 +11,185 @@ export function createFunc(){
     var showNum = 12;
 
     if(calBox){
+
         showProcess(today);
-    }
 
-    /*-----------------------------------------
-    年間カレンダー作成
-    -----------------------------------------*/
-    function showProcess(date) {
+        /*-----------------------------------------
+        年間カレンダー作成
+        -----------------------------------------*/
+        function showProcess(date) {
 
-        var year = date.getFullYear();
-        var month = date.getMonth();
+            var year = date.getFullYear();
+            var month = date.getMonth();
 
-        for(let i = 0; i <showNum ; i++ ){
+            for(let i = 0; i <showNum ; i++ ){
 
-            // 1月分のカレンダーwrap
-            var calBoxInner = document.createElement('div');
-            calBoxInner.className = 'rc-calbox';
+                // 1月分のカレンダーwrap
+                var calBoxInner = document.createElement('div');
+                calBoxInner.className = 'rc-calbox';
 
-            if(month + i + 1 <= 12){
-                calBoxInner.innerHTML = '<div class="rc-calbox__header"><h2 class="rc-calbox__ttl">' + year + "年 " + (month + i + 1) + "月" + '</h2><label><input type="checkbox" name="calAllChangeCheckboxMonth" value="allCheckFlg">この月をすべて選択/解除</label></div>';
-            }else{
-                calBoxInner.innerHTML = '<div class="rc-calbox__header"><h2 class="rc-calbox__ttl">' + (year+1) + "年 " + (month + i + 1 - 12) + "月" + '</h2><label><input type="checkbox" name="calAllChangeCheckboxMonth" value="allCheckFlg">この月をすべて選択/解除</label></div>';
-            }
-
-            var calBoxTable = document.createElement('table');
-            calBoxTable.innerHTML += createProcess(year, month + i);
-
-            calBoxInner.appendChild(calBoxTable);
-            calBox.appendChild(calBoxInner);
-        }
-    }
-
-    /*-----------------------------------------
-    カレンダーテーブル生成
-    -----------------------------------------*/
-    function createProcess(year, month) {
-        
-        if(typeof setting_records_array !== 'undefined'){
-            let rc_statelist = setting_records_array;
-
-            var calendar = "<tr class='dayOfWeek'>";
-
-            for (var i = 0; i < week.length; i++) {
-                calendar += "<th>" + week[i] + "</th>";
-            }
-
-            calendar += "</tr>";
-
-            var count = 0;
-            var startDayOfWeek = new Date(year, month, 1).getDay();
-            var endDate = new Date(year, month + 1, 0).getDate();
-            var lastMonthEndDate = new Date(year, month, 0).getDate();
-            var row = Math.ceil((startDayOfWeek + endDate) / week.length);
-
-            // イベント追加ボタン生成関数
-            var buttonDomCreate = function(year,month,day){
-                var events_name = 'rc_events_' + year + "-" + month + "-" + day;
-                return "<a class='rc-addbtn' data-date='" + year + "-" + month + "-" + day + "' data-meta="+ events_name +">+</a><input type='checkbox' name='allset'>";
-            }
-
-            // セレクトボタン生成関数
-            var selectCreate = function(rc_statelist,year,month,day){
-                let option_list = '<option value="">--</option>';
-                // ex) rc_status_2022-02-04
-                let status_name = 'rc_status_' + year + "-" + month + "-" + day;
-                // rc_data_arrayはcalendar.phpに記載
-                let target_date = rc_status_array.find((v) => v.meta_key === status_name);
-
-                for(let i = 0; i < rc_statelist.length; i++){
-                    let selected_txt = '';
-                    if(target_date){
-                        selected_txt = (target_date.meta_value == rc_statelist[i].state_name)? 'selected' : '';
-                    }
-                    option_list += '<option value="'+rc_statelist[i].state_name+'"'+ selected_txt +'>'+rc_statelist[i].state_name+'</option>';
-                }
-                
-                return "<select class='rc_status_selectbtn' name='"+ status_name +"'>" + option_list + "</select>";
-            }
-
-            // イベント生成
-            var eventCreate = function(year,month,day){
-                var events_name = 'rc_events_' + year + "-" + month + "-" + day;
-                var target_date = rc_events_array.find((v) => v.meta_key === events_name);
-                var events_list = '';
-
-                if(target_date){
-                    var events_list_arry = JSON.parse(target_date.meta_value);
-                    for(let i = 0; i < Object.keys(events_list_arry).length; i++){
-                        events_list += 
-                            '<a class="rc-event__btn" data-date="' +year + '-' + month + '-' + day + '" data-meta='+ events_name +' data-eventnum=' + i + '>' + events_list_arry[i].event_name + '</a>';
-                    }
+                if(month + i + 1 <= 12){
+                    calBoxInner.innerHTML = '<div class="rc-calbox__header"><h2 class="rc-calbox__ttl">' + year + "年 " + (month + i + 1) + "月" + '</h2><label><input type="checkbox" name="calAllChangeCheckboxMonth" value="allCheckFlg">この月をすべて選択/解除</label></div>';
                 }else{
-                    events_list = '';
+                    calBoxInner.innerHTML = '<div class="rc-calbox__header"><h2 class="rc-calbox__ttl">' + (year+1) + "年 " + (month + i + 1 - 12) + "月" + '</h2><label><input type="checkbox" name="calAllChangeCheckboxMonth" value="allCheckFlg">この月をすべて選択/解除</label></div>';
                 }
 
-                return events_list;
-            }
+                var calBoxTable = document.createElement('table');
+                calBoxTable.innerHTML += createProcess(year, month + i);
 
-            // カレンダー生成
-            for (var i = 0; i < row; i++) {
-                calendar += "<tr>";
-                for (var j = 0; j < week.length; j++) {
-                    if (i == 0 && j < startDayOfWeek) {
-                        // 前月の日付部分生成
-                        calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
-                    } else if (count >= endDate) {
-                        // 翌月の日付部分生成
-                        count++;
-                        var counta = count - endDate;
-                        counta = counta.toString().padStart(2,'0');
-                        calendar += "<td class='disabled'>" + "<span>" + counta + "</span>" + "</td>";
-                    } else {
-                        count++;
-                        if(year == today.getFullYear() && month == (today.getMonth()) && count == today.getDate()){
-                            // 当日生成
-                            var counta = count;
-                            var montha = month + 1;
+                calBoxInner.appendChild(calBoxTable);
+                calBox.appendChild(calBoxInner);
+            }
+        }
+
+        /*-----------------------------------------
+        カレンダーテーブル生成
+        -----------------------------------------*/
+        function createProcess(year, month) {
+            
+            if(typeof setting_records_array !== 'undefined'){
+                let rc_statelist = setting_records_array;
+
+                var calendar = "<tr class='dayOfWeek'>";
+
+                for (var i = 0; i < week.length; i++) {
+                    calendar += "<th>" + week[i] + "</th>";
+                }
+
+                calendar += "</tr>";
+
+                var count = 0;
+                var startDayOfWeek = new Date(year, month, 1).getDay();
+                var endDate = new Date(year, month + 1, 0).getDate();
+                var lastMonthEndDate = new Date(year, month, 0).getDate();
+                var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+                // イベント追加ボタン生成関数
+                var buttonDomCreate = function(year,month,day){
+                    var events_name = 'rc_events_' + year + "-" + month + "-" + day;
+                    return "<a class='rc-addbtn' data-date='" + year + "-" + month + "-" + day + "' data-meta="+ events_name +">+</a><input type='checkbox' name='allset'>";
+                }
+
+                // セレクトボタン生成関数
+                var selectCreate = function(rc_statelist,year,month,day){
+                    let option_list = '<option value="">--</option>';
+                    // ex) rc_status_2022-02-04
+                    let status_name = 'rc_status_' + year + "-" + month + "-" + day;
+                    // rc_data_arrayはcalendar.phpに記載
+                    let target_date = rc_status_array.find((v) => v.meta_key === status_name);
+
+                    for(let i = 0; i < rc_statelist.length; i++){
+                        let selected_txt = '';
+                        if(target_date){
+                            selected_txt = (target_date.meta_value == rc_statelist[i].state_name)? 'selected' : '';
+                        }
+                        option_list += '<option value="'+rc_statelist[i].state_name+'"'+ selected_txt +'>'+rc_statelist[i].state_name+'</option>';
+                    }
+                    
+                    return "<select class='rc_status_selectbtn' name='"+ status_name +"'>" + option_list + "</select>";
+                }
+
+                // イベント生成
+                var eventCreate = function(year,month,day){
+                    var events_name = 'rc_events_' + year + "-" + month + "-" + day;
+                    var target_date = rc_events_array.find((v) => v.meta_key === events_name);
+                    var events_list = '';
+
+                    if(target_date){
+                        var events_list_arry = JSON.parse(target_date.meta_value);
+                        for(let i = 0; i < Object.keys(events_list_arry).length; i++){
+                            events_list += 
+                                '<a class="rc-event__btn" data-date="' +year + '-' + month + '-' + day + '" data-meta='+ events_name +' data-eventnum=' + i + '>' + events_list_arry[i].event_name + '</a>';
+                        }
+                    }else{
+                        events_list = '';
+                    }
+
+                    return events_list;
+                }
+
+                // カレンダー生成
+                for (var i = 0; i < row; i++) {
+                    calendar += "<tr>";
+                    for (var j = 0; j < week.length; j++) {
+                        if (i == 0 && j < startDayOfWeek) {
+                            // 前月の日付部分生成
+                            calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+                        } else if (count >= endDate) {
+                            // 翌月の日付部分生成
+                            count++;
+                            var counta = count - endDate;
                             counta = counta.toString().padStart(2,'0');
-                            montha = montha.toString().padStart(2,'0');
-                            calendar += "<td style='background:#ffdddd;'>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
-                        }else if(year == today.getFullYear() && month == (today.getMonth()) && count < today.getDate()){
-                            // 当月の当日より前の日（過ぎてしまった日）
-                            var counta = count;
-                            var montha = month + 1;
-                            counta = counta.toString().padStart(2,'0');
-                            montha = montha.toString().padStart(2,'0');
-                            calendar += "<td>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
-                        }else{
-                            var counta = count;
-                            counta = counta.toString().padStart(2,'0');
-                            if(month + 1 <= 12){
-                                // 当日以降
+                            calendar += "<td class='disabled'>" + "<span>" + counta + "</span>" + "</td>";
+                        } else {
+                            count++;
+                            if(year == today.getFullYear() && month == (today.getMonth()) && count == today.getDate()){
+                                // 当日生成
+                                var counta = count;
                                 var montha = month + 1;
+                                counta = counta.toString().padStart(2,'0');
+                                montha = montha.toString().padStart(2,'0');
+                                calendar += "<td style='background:#ffdddd;'>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
+                            }else if(year == today.getFullYear() && month == (today.getMonth()) && count < today.getDate()){
+                                // 当月の当日より前の日（過ぎてしまった日）
+                                var counta = count;
+                                var montha = month + 1;
+                                counta = counta.toString().padStart(2,'0');
                                 montha = montha.toString().padStart(2,'0');
                                 calendar += "<td>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
                             }else{
-                                // 翌年
-                                var montha = month + 1 - 12;
-                                montha = montha.toString().padStart(2,'0');
-                                calendar += "<td>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
+                                var counta = count;
+                                counta = counta.toString().padStart(2,'0');
+                                if(month + 1 <= 12){
+                                    // 当日以降
+                                    var montha = month + 1;
+                                    montha = montha.toString().padStart(2,'0');
+                                    calendar += "<td>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
+                                }else{
+                                    // 翌年
+                                    var montha = month + 1 - 12;
+                                    montha = montha.toString().padStart(2,'0');
+                                    calendar += "<td>" + "<span>" + count + "</span>" + selectCreate(rc_statelist,year,montha,counta) + eventCreate(year,montha,counta) + buttonDomCreate(year,montha,counta) + "</td>";
+                                }
                             }
                         }
                     }
+                    calendar += "</tr>";
                 }
-                calendar += "</tr>";
+
+                return calendar;
             }
 
-            return calendar;
         }
 
+        // 生成したカレンダーのプラスボタンをクリックイベントを設定
+        function rcBtnAction(){
+
+            var rc_addbtn = document.getElementsByClassName('rc-addbtn');
+
+            if(rc_addbtn.length !== 0){
+                for(let i = 0; i < rc_addbtn.length; i++){
+                    rc_addbtn[i].addEventListener('click',function(){
+                        let date = this.dataset.date;
+                        let meta = this.dataset.meta;
+                        create_modal_window.open(date,meta);
+                    })
+                }
+        
+                var rc_event__btn = document.getElementsByClassName('rc-event__btn');
+                for(let i = 0; i < rc_event__btn.length; i++){
+                    rc_event__btn[i].addEventListener('click',function(){
+                        let date = this.dataset.date;
+                        let meta = this.dataset.meta;
+                        let num = this.dataset.eventnum;
+                        create_modal_window.open(date,meta,num);
+                    })
+                }
+            }
+        }
+
+        rcBtnAction();
     }
 
-    // 生成したカレンダーのプラスボタンをクリックイベントを設定
-    function rcBtnAction(){
-
-        var rc_addbtn = document.getElementsByClassName('rc-addbtn');
-
-        for(let i = 0; i < rc_addbtn.length; i++){
-            rc_addbtn[i].addEventListener('click',function(){
-                let date = this.dataset.date;
-                let meta = this.dataset.meta;
-                create_modal_window.open(date,meta);
-            })
-        }
-
-        var rc_event__btn = document.getElementsByClassName('rc-event__btn');
-        for(let i = 0; i < rc_event__btn.length; i++){
-            rc_event__btn[i].addEventListener('click',function(){
-                let date = this.dataset.date;
-                let meta = this.dataset.meta;
-                let num = this.dataset.eventnum;
-                create_modal_window.open(date,meta,num);
-            })
-        }
-    }
-
-    rcBtnAction();
-}
-
-export default function(){
-
+    
 }
--- src/js/modules/create-modal-window.js
+++ src/js/modules/create-modal-window.js
@@ -4,113 +4,118 @@ export function open(date,meta,num){
 
     var cal_container = document.getElementsByName('cal_container')[0];
     var eventDomContent = '';
-    var target_events = rc_events_array.find((v) => v.meta_key === meta);
+    if(typeof rc_events_array !== 'undefined'){
+        var target_events = rc_events_array.find((v) => v.meta_key === meta);
+    }
     var events_list_arry;
 
-    var addView = (date,meta,num) =>{
-        eventDomContent += 
-            "<div class='rc-modal__wrap'>" +
-                "<p class='rc-modal__ttl'>"+date+"</p>" +
-                "<table class='rc-modal__inner'>" +
+    if(cal_container){
+        var addView = (date,meta,num) =>{
+            eventDomContent += 
+                "<div class='rc-modal__wrap'>" +
+                    "<p class='rc-modal__ttl'>"+date+"</p>" +
+                    "<table class='rc-modal__inner'>" +
+                        "<tr>" +
+                        "<th>タイトル</th>" +
+                        "<td><input type='text' name='"+ meta +"["+num+"][event_name]' value=''></td>" +
+                        "</tr>" +
+                        "<tr>" +
+                        "<th>URL</th>" +
+                        "<td><input type='text' name='"+ meta +"["+num+"][event_url]' value=''></td>" +
+                        "</tr>" +
+                        "<tr>" +
+                        "<th>カラー</th>" +
+                        "<td><input type='color' name='"+ meta +"["+num+"][event_color]' value='#b1fcb0'></td>" +
+                        "</tr>" +
+                    "</table>" +
+                    "<div class='rc-modal__submit'><input type='submit' value='追加する' name='save'></div>" + 
+                    "<a class='rc-modal__close'>閉じる</a>" +
+                "</div>";
+        }
+    
+        var rewriteView = (date,meta,num) =>{
+            eventDomContent += 
+                "<div class='rc-modal__wrap'>" +
+                    "<p class='rc-modal__ttl'>"+date+"</p>" +
+                    "<table class='rc-modal__inner'>" +
                     "<tr>" +
                     "<th>タイトル</th>" +
-                    "<td><input type='text' name='"+ meta +"["+num+"][event_name]' value=''></td>" +
+                    "<td><input type='text' name='" + meta + "["+num+"][event_name]' value='"+ events_list_arry[num].event_name + "'></td>" +
                     "</tr>" +
                     "<tr>" +
                     "<th>URL</th>" +
-                    "<td><input type='text' name='"+ meta +"["+num+"][event_url]' value=''></td>" +
+                    "<td><input type='text' name='" + meta + "["+num+"][event_url]' value='" + events_list_arry[num].event_url + "'></td>" +
                     "</tr>" +
                     "<tr>" +
                     "<th>カラー</th>" +
-                    "<td><input type='color' name='"+ meta +"["+num+"][event_color]' value='#b1fcb0'></td>" +
+                    "<td><input type='color' name='" + meta + "["+num+"][event_color]' value='" + events_list_arry[num].event_color + "'></td>" +
                     "</tr>" +
-                "</table>" +
-                "<div class='rc-modal__submit'><input type='submit' value='追加する' name='save'></div>" + 
-                "<a class='rc-modal__close'>閉じる</a>" +
-            "</div>";
-    }
-
-    var rewriteView = (date,meta,num) =>{
-        eventDomContent += 
-            "<div class='rc-modal__wrap'>" +
-                "<p class='rc-modal__ttl'>"+date+"</p>" +
-                "<table class='rc-modal__inner'>" +
-                "<tr>" +
-                "<th>タイトル</th>" +
-                "<td><input type='text' name='" + meta + "["+num+"][event_name]' value='"+ events_list_arry[num].event_name + "'></td>" +
-                "</tr>" +
-                "<tr>" +
-                "<th>URL</th>" +
-                "<td><input type='text' name='" + meta + "["+num+"][event_url]' value='" + events_list_arry[num].event_url + "'></td>" +
-                "</tr>" +
-                "<tr>" +
-                "<th>カラー</th>" +
-                "<td><input type='color' name='" + meta + "["+num+"][event_color]' value='" + events_list_arry[num].event_color + "'></td>" +
-                "</tr>" +
-                "</table>" +
-                "<div class='rc-modal__submit'><input type='submit' value='更新する' name='save'></div>" + 
-                "<div class='rc-modal__controller'>" +
-                    "<a class='rc-modal__delete'>削除</a>" + 
-                "</div>" +
-                "<a class='rc-modal__close'>閉じる</a>" +
-            "</div>";
-    }
-
-    if(target_events){
-        events_list_arry = JSON.parse(target_events.meta_value);
-
-        // データがある場合
-        if(num){
-            rewriteView(date,meta,num);
-        }else{
-            // 最後に追加
-            var eventNum = Object.keys(events_list_arry).length;
-            addView(date,meta,eventNum);
+                    "</table>" +
+                    "<div class='rc-modal__submit'><input type='submit' value='更新する' name='save'></div>" + 
+                    "<div class='rc-modal__controller'>" +
+                        "<a class='rc-modal__delete'>削除</a>" + 
+                    "</div>" +
+                    "<a class='rc-modal__close'>閉じる</a>" +
+                "</div>";
         }
-
-        for(let i = 0; i < Object.keys(events_list_arry).length; i++){
-            num = Number(num);
     
-            if(i !== num){
-                eventDomContent +=
-                    "<input type='hidden' name='" + meta + "["+i+"]" +"[event_name]' value='" + events_list_arry[i].event_name + "'>";
-                eventDomContent +=
-                    "<input type='hidden' name='" + meta + "["+i+"]" +"[event_url]' value='" + events_list_arry[i].event_url + "'>";
-                eventDomContent +=
-                    "<input type='hidden' name='" + meta + "["+i+"]" +"[event_color]' value='" + events_list_arry[i].event_color + "'>";
+        if(target_events){
+            events_list_arry = JSON.parse(target_events.meta_value);
+    
+            // データがある場合
+            if(num){
+                rewriteView(date,meta,num);
+            }else{
+                // 最後に追加
+                var eventNum = Object.keys(events_list_arry).length;
+                addView(date,meta,eventNum);
             }
+    
+            for(let i = 0; i < Object.keys(events_list_arry).length; i++){
+                num = Number(num);
+        
+                if(i !== num){
+                    eventDomContent +=
+                        "<input type='hidden' name='" + meta + "["+i+"]" +"[event_name]' value='" + events_list_arry[i].event_name + "'>";
+                    eventDomContent +=
+                        "<input type='hidden' name='" + meta + "["+i+"]" +"[event_url]' value='" + events_list_arry[i].event_url + "'>";
+                    eventDomContent +=
+                        "<input type='hidden' name='" + meta + "["+i+"]" +"[event_color]' value='" + events_list_arry[i].event_color + "'>";
+                }
+            }
+    
+        }else{
+            // データがない場合（追加）
+            addView(date,meta,0);
         }
-
-    }else{
-        // データがない場合（追加）
-        addView(date,meta,0);
+    
+    
+        var eventDomWrap = document.createElement('div');
+        eventDomWrap.className = 'rc-cal__inputwrap';
+        eventDomWrap.innerHTML += eventDomContent;
+        cal_container.appendChild(eventDomWrap);
+    
+        // close
+        var rcModalClose = document.getElementsByClassName('rc-modal__close')[0];
+        rcModalClose.addEventListener('click', function(){
+            close();
+        })
+    
+        // delete
+        var rcModalDelete = document.getElementsByClassName('rc-modal__delete')[0];
+        if(rcModalDelete){
+            rcModalDelete.addEventListener('click', function(){
+                inputDelete(meta,num);
+            });
+        }
     }
-
-
-    var eventDomWrap = document.createElement('div');
-    eventDomWrap.className = 'rc-cal__inputwrap';
-    eventDomWrap.innerHTML += eventDomContent;
-    cal_container.appendChild(eventDomWrap);
-
-    // close
-    var rcModalClose = document.getElementsByClassName('rc-modal__close')[0];
-    rcModalClose.addEventListener('click', function(){
-        close();
-    })
-
-    // delete
-    var rcModalDelete = document.getElementsByClassName('rc-modal__delete')[0];
-    if(rcModalDelete){
-        rcModalDelete.addEventListener('click', function(){
-            inputDelete(meta,num);
-        });
-    }
-
 }
 
 export function close(){
     var rc_cal_inputwrap = document.getElementsByClassName('rc-cal__inputwrap')[0];
-    rc_cal_inputwrap.remove();
+    if(rc_cal_inputwrap){
+        rc_cal_inputwrap.remove();
+    }
 }
 
 export function inputDelete(meta,num){
